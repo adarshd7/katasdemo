ea2686250bac1d9fc43c1635ceac36b961a10ba2
adarshd7 <adarsh.deekonda@zemosolabs.com>
2022-01-10 12:34:33 +0530

    conflict.py has been changed

--- mergeconflict/conflict.py
+++ mergeconflict/conflict.py
@@ -1,9 +1,9 @@
-def Cloning(li1):
-    li_copy = li1[:]
+def Class(li1):
+    li = li1[:]
     return list
    
 
 lit = [4, 8, 2, 10, 15, 18]
 li = Cloning(li1)
-print("Orig List:", li1)
+print("Oreo List:", li1)
 print("After Clon:", li2)
