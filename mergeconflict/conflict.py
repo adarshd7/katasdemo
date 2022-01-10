f5274662e6991847e81a12dcae5ff2607e400aa4
adarshd7 <adarsh.deekonda@zemosolabs.com>
2022-01-10 08:13:44 +0530

    modifyfile

--- mergeconflict/conflict.py
+++ mergeconflict/conflict.py
@@ -1,9 +1,9 @@
 def Cloning(li1):
     li_copy = li1[:]
-    return li_copy
+    return list
    
 
-li1 = [4, 8, 2, 10, 15, 18]
-li2 = Cloning(li1)
-print("Original List:", li1)
-print("After Cloning:", li2)
+lit = [4, 8, 2, 10, 15, 18]
+li = Cloning(li1)
+print("Orig List:", li1)
+print("After Clon:", li2)
